6d8767ecde36fb7face89094dae521c3224871ed
Sean Kramer <seandominickramer@gmail.com>
2020-04-07 18:24:26 -0400

    Add 'else' parts to background color functions to undo colors when scrolling up

--- js/index.js
+++ js/index.js
@@ -20,17 +20,31 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     get('.intro').style.backgroundColor = 'orange';
+  }else{
+    get('.intro').style.backgroundColor = '';
   };
+
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     get('.content-section').style.backgroundColor = 'pink';
+  }else{
+    get('.content-section').style.backgroundColor = '';
   };
+
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     get('.inverse-content').style.backgroundColor = 'aqua';
+  }else{
+    get('.inverse-content').style.backgroundColor = '';
   };
+
   if (document.body.scrollTop > 700|| document.documentElement.scrollTop > 700) {
     get('.content-destination').style.backgroundColor = 'red';
+  }else{
+    get('.content-destination').style.backgroundColor = '';
   };
+
   if (document.body.scrollTop > 900|| document.documentElement.scrollTop > 900) {
     get('.content-pick').style.backgroundColor = 'green';
+  }else{
+    get('.content-pick').style.backgroundColor = ''; 
   };
 }
